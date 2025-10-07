a4af59df27455ac0265f6394f6dd5e495519f054
agfonse2 <valen2008dl@gmail.com>
2025-10-06 19:38:36 -0500

    bd

--- sipavagt.sql
+++ sipavagt.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 23-09-2025 a las 22:07:36
+-- Tiempo de generación: 07-10-2025 a las 02:38:05
 -- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.0.30
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -86,11 +86,21 @@ CREATE TABLE `producto` (
   `id_producto` int(11) NOT NULL,
   `nombre_producto` varchar(80) DEFAULT NULL,
   `precio` decimal(10,2) DEFAULT NULL,
+  `cantidad` int(11) NOT NULL,
+  `imagen` varchar(255) NOT NULL,
   `fecha_elaboracion` date DEFAULT NULL,
   `id_catalogo` int(11) DEFAULT NULL,
   `id_inventario` int(11) DEFAULT NULL,
   `id_categoria` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`id_producto`, `nombre_producto`, `precio`, `cantidad`, `imagen`, `fecha_elaboracion`, `id_catalogo`, `id_inventario`, `id_categoria`) VALUES
+(1, 'ad', 12.00, 12, 's-l1600.webp', NULL, NULL, NULL, NULL),
+(2, 'zxczx', 123.00, 4, 'collar-para-dama-collares-mayoreo-bisuteria-fina-D_NQ_NP_139425-MLM25445564656_032017-F.jpg', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -149,15 +159,15 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellido`, `telefono`, `correo_electronico`, `cedula`, `fecha_registro`, `clave`, `id_rol`) VALUES
-(3, 'david', 'perez', '3227122202', 'usechedavid525@gmail.com', 1023901661, NULL, 'scrypt:32768:8:1$QHThX4PfuL7LIJX1$f8e29df003c8e16e9555604c44693a3d2ab8f0145452fa76821a63cc28e3b3e5db9410933f281888643fa383f1469885421069a3658930a0bc643f41a8053ea9', NULL),
+(3, 'david', 'perezfg', '3227122202', 'usechedavid525@gmail.com', 1023901661, NULL, 'scrypt:32768:8:1$QHThX4PfuL7LIJX1$f8e29df003c8e16e9555604c44693a3d2ab8f0145452fa76821a63cc28e3b3e5db9410933f281888643fa383f1469885421069a3658930a0bc643f41a8053ea9', 1),
 (4, 'helen', 'silva', '3227122202', 'helensilvabravo@gmial.com', 1023901661, NULL, 'scrypt:32768:8:1$pdJdYzMFUFmW5Zd2$90d0f975fcc3de0397d33de03888da802f532f077fa67d9ef12a3692f4c37680cf3314ee653bbd230e0d0bee6556b36dc532c250da69c46671918d2052ddee39', NULL),
-(5, 'dayanna', 'ramirez', '3222352746', 'dvrg2612ramirez@gmail.com', 1021679171, NULL, 'scrypt:32768:8:1$1uT4FzfhE73RuFqZ$011407640b132bd5fe84210055dbd842caa57682c57215a1f7ef588c660e3e2fe4b18e6eedfc3e98a27183323b8d06eab2132779fea8b281e7c6329354b6eaba', NULL),
-(6, 'a', 'a', '1234', 'a@a', 123456, NULL, 'scrypt:32768:8:1$ERpyi21nAXtMWfoL$0296e1d3aa5014d304fcb46f19016779cbc58d58979ad9b03f5aae8851490b73037185dccb1584c1e9824a47c3ea59c6533d37017920d0929fcd70035b1c68ff', 1),
-(7, 'aaaaaaa', 'sss', '211223', 'dsassx@ghg', 22226661, NULL, 'scrypt:32768:8:1$1maJQNsLrz3X5VJe$cc316826e0d6c9c5ae611f0e36c2d7f9c7375b602a7febe8c5816a7d672ead02e56ddcaee389b508a1efaaa832664ab5ce63cd74ecdc68bfbc6f27ee1c60074a', NULL),
-(8, 'dddd', ' cccc', '23445', 'hshsgbs@jaj', 12678, NULL, 'scrypt:32768:8:1$Yg5DDvwl1jptD526$acc497910682cff717db0b1fb73063ddb5e38fabc692fb3f1d2bd6afcfe91c826b7c62908ed3852c7c9b6a663967ba6024cd0b101b4e95e4dddb1a379d16bef5', NULL),
-(11, 'aaaaaaa', 'sss', '211223', 'dsassx@ghgK', 22226661, NULL, 'scrypt:32768:8:1$cTh5zLQ6fLZpZ551$e7435e1ff07104fcb13f060b271a0035e1430f30f6c5dd675d67df816fd92a3923cc2dded0a1bedfca66ed542518838d6e3a9a12573cfb4c36d80988e3f2634c', NULL),
-(12, 'eeeeeee', 'eeeeeee', 'eeeeeee', 'eeeeeeeeeee@eeee', 0, NULL, 'scrypt:32768:8:1$YWaJNR7V6Er84Din$f2f00137af5390f82e927741a6bb6c52f775903b95ba47d6f653b92b3091a1e38cdf7fe07ae8ca42da734a2947c8582870c4246394744201dbd6e161eaaf6657', NULL),
-(13, 'bbbb', 'bbbb', 'bbbbb', 'bbbbbb@vvvvv', 223334444, NULL, 'scrypt:32768:8:1$7kki1bsWf0KrogwU$4ad959f1c6efb273b3ddc93fbfaf17e5c4f55da216ad97ad0b75ac93c47471de813025a54fcb6ac25c0364d83545830e62397b90949bc37d08d7cf54706dc4eb', NULL);
+(5, 'dayanna', 'ramirez', '3222352746', 'dvrg2612ramirez@gmail.com', 1021679171, NULL, 'scrypt:32768:8:1$1uT4FzfhE73RuFqZ$011407640b132bd5fe84210055dbd842caa57682c57215a1f7ef588c660e3e2fe4b18e6eedfc3e98a27183323b8d06eab2132779fea8b281e7c6329354b6eaba', 2),
+(6, 'angela', 'fonseca', '1234', 'a@gmail.com', 123456, NULL, 'scrypt:32768:8:1$ERpyi21nAXtMWfoL$0296e1d3aa5014d304fcb46f19016779cbc58d58979ad9b03f5aae8851490b73037185dccb1584c1e9824a47c3ea59c6533d37017920d0929fcd70035b1c68ff', 1),
+(7, 'aaaaaaa', 'sss', '211223', 'dsassx@ghg', 22226661, NULL, 'scrypt:32768:8:1$1maJQNsLrz3X5VJe$cc316826e0d6c9c5ae611f0e36c2d7f9c7375b602a7febe8c5816a7d672ead02e56ddcaee389b508a1efaaa832664ab5ce63cd74ecdc68bfbc6f27ee1c60074a', 2),
+(11, 'vvvvv', 'vvvvv', '211223', 'fdsfsd@dfdsfgsd', 22226661, NULL, 'scrypt:32768:8:1$cTh5zLQ6fLZpZ551$e7435e1ff07104fcb13f060b271a0035e1430f30f6c5dd675d67df816fd92a3923cc2dded0a1bedfca66ed542518838d6e3a9a12573cfb4c36d80988e3f2634c', 2),
+(13, 'bbbb', 'bbbb', 'bbbbb', 'bbbbbb@vvvvv', 223334444, NULL, 'scrypt:32768:8:1$7kki1bsWf0KrogwU$4ad959f1c6efb273b3ddc93fbfaf17e5c4f55da216ad97ad0b75ac93c47471de813025a54fcb6ac25c0364d83545830e62397b90949bc37d08d7cf54706dc4eb', NULL),
+(14, 'a', 'a', '123', 'a@a', NULL, NULL, 'scrypt:32768:8:1$y6FEar36eRYLgFcY$083d1994990815147d27e8d20d2853426afcb98935ef92fd2059d3fa9dc009bc38ebf041268ff3245503c4b6cbd1d7bda6b7e0afa11db06185a68e475a9a32db', 2),
+(15, 'asd', 'asd', '123', 'asd@asd', NULL, NULL, NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -272,7 +282,7 @@ ALTER TABLE `metodo_pago`
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `proveedor`
@@ -290,7 +300,7 @@ ALTER TABLE `rol_usuario`
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `venta`
@@ -308,27 +318,6 @@ ALTER TABLE `venta`
 ALTER TABLE `envio`
   ADD CONSTRAINT `envio_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`),
   ADD CONSTRAINT `envio_ibfk_2` FOREIGN KEY (`id_metodo`) REFERENCES `metodo_pago` (`id_metodo`);
-
---
--- Filtros para la tabla `producto`
---
-ALTER TABLE `producto`
-  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_catalogo`) REFERENCES `catalogo` (`id_catalogo`),
-  ADD CONSTRAINT `producto_ibfk_2` FOREIGN KEY (`id_inventario`) REFERENCES `inventario` (`id_inventario`);
-
---
--- Filtros para la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD CONSTRAINT `usuario_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol_usuario` (`id_rol`);
-
---
--- Filtros para la tabla `venta`
---
-ALTER TABLE `venta`
-  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`),
-  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
-  ADD CONSTRAINT `venta_ibfk_3` FOREIGN KEY (`id_factura`) REFERENCES `factura` (`id_factura`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
